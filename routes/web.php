402ee6d5f5a5ef75776ff832ac912b41612ba024
AkhsanDaffa <akhsandaffa01@gmail.com>
2025-12-11 09:22:41 +0700

    Add Simple Input

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-input', function () {
+    try {
+        User::create([
+            'name' => 'Daffa DevOps',
+            'email' => 'Daffa@devops.com',
+            'password' => Hash::make('rahasia123'),
+        ]);
+        return "<h1>✅ SUKSES! Data 'Daffa DevOps' berhasil masuk ke Database.</h1>";
+    } catch (\Exception $e) {
+        return "<h1>❌ GAGAL: " . $e->getMessage() . "</h1>";
+    }
 });
